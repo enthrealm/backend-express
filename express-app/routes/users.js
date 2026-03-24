c32401619498dc3bd1be9ab78c3de3f233546acd
ENTHREALM <temnyakovaanna2006@gmail.com>
2026-03-24 17:46:43 +0500

    kjdskjf

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -16,6 +16,15 @@ router.get('/', function(req, res, next) {
   res.send({ items: users });
 });
 
+router.get('/:id', (req, res, next) => {
+  const id = parseInt(req.params.id);
+  const user = users.find(u => u.id === id);
+  if (!user) {
+    return res.status(404);
+  }
+  res.json(user);
+})
+
 router.post('/', function(req, res, next) {
   const newUser = req.body;
   users.push(newUser);
