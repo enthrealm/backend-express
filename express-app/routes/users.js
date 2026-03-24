22e83ecca6c8f655a8bd9af42172e08f02ed0104
ENTHREALM <temnyakovaanna2006@gmail.com>
2026-03-24 17:30:24 +0500

    jdkjf

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+  {
+    id: 1,
+    name: "Темнякова Анна"
+  },
+  {
+    id: 2,
+    name: "Мартемьянова Вера"
+  }
+];
+
 router.get('/', function(req, res, next) {
-  const users = [
-    {
-      id: 1,
-      name: "Темнякова Анна"
-    },
-    {
-      id: 2,
-      name: "Мартемьянова Вера"
-    }
-  ];
-  
-  res.send({
-    items: users
-  });
+  res.send({ items: users });
 });
 
+router.post('/', function(req, res, next) {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+})
 module.exports = router;
+
